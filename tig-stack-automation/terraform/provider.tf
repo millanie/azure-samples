c805be7479d0acb94f7f201f7c231fd2a44e2ae2
kate <23490278+millanie@users.noreply.github.com>
2020-03-03 11:57:19 +0000

    add terraform script for tig stack

---
+++ tig-stack-automation/terraform/provider.tf
@@ -0,0 +1,13 @@
+### Azure Provider
+provider "azurerm" {
+  version = "2.0.0"
+  # version = ">= 1.0.0"
+  features {}
+ 
+  # subscription_id = var.subscription_id
+  # client_id = var.client_id
+  # client_secret = var.client_secret
+  # tenant_id = var.tenant_id
+  
+}
+
---
+++ tig-stack-automation/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {default = "Terraform"}
+variable "location" {default = ""}
+
+variable "vnet_name" {default = ""}
+variable "vnet_cidr" {default = ""}
+variable "subnet_prefixes" {default = []}
+
+variable "password" {default = ""}
+variable "admin_user" {default = "azuser"}
+
+variable "vm_name" {default = []}
+variable "vm_sku" {default = "Standard_DS1_V2"}
+
+variable "os_sku" {default = []}
+
+variable "jumpbox_ip" {default = ""}
+variable "jumpbox_port" {default = ""}
+variable "rule_name" {default = ""}
+
