75c290f7674d59b6f9223aaf2da529f837bf670a
kate <23490278+millanie@users.noreply.github.com>
2020-03-16 03:30:10 +0000

    add windows vm

--- tig-stack-automation/terraform/vm_windows.tf
+++ tig-stack-automation/terraform/vm_windows.tf
@@ -1,32 +1,32 @@
-resource "random_string" "fqdn" {
+resource "random_string" "fqdn_win" {
  length  = 6
  special = false
  upper   = false
  number  = false
 }
 
-resource "azurerm_public_ip" "monitoring" {
+resource "azurerm_public_ip" "terraform" {
   name = "${var.vm_name}-pip"
   resource_group_name = var.resource_group_name
   location = var.location
   allocation_method = "Static"
-  domain_name_label = random_string.fqdn.result
+  domain_name_label = random_string.fqdn_win.result
 }
 
-resource "azurerm_network_interface" "monitoring" {
-  name                = "monitoring-nic"
+resource "azurerm_network_interface" "terraform" {
+  name                = "${var.vm_name}-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
 
   ip_configuration {
-    name                          = "monitoring-nic-config"
+    name                          = "${var.vm_name}-nic-config"
     subnet_id                     = module.network.vnet_subnets[0]
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.monitoring.id
+    public_ip_address_id = azurerm_public_ip.terraform.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "monitoring" {
+resource "azurerm_windows_virtual_machine" "terraform" {
   name                = var.vm_name 
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -34,15 +34,9 @@ resource "azurerm_linux_virtual_machine" "monitoring" {
 
   admin_username      = var.admin_user
   admin_password      = var.password
-  disable_password_authentication = false 
   network_interface_ids = [
-    azurerm_network_interface.monitoring.id,
+    azurerm_network_interface.terraform.id,
   ]
-
-  # admin_ssh_key {
-  #   username   = "adminuser"
-  #   public_key = file("~/.ssh/id_rsa.pub")
-  # }
 
   os_disk {
     caching              = "ReadWrite"
@@ -57,37 +51,23 @@ resource "azurerm_linux_virtual_machine" "monitoring" {
   }
 }
 
-resource "azurerm_network_security_group" "monitoring" {
+resource "azurerm_network_security_group" "terraform" {
   name = "${var.vm_name}-nsg"
   resource_group_name = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_network_security_rule" "monitoring" {
+resource "azurerm_network_security_rule" "terraform" {
     name = var.rule_name
     priority = 100
     direction = "Inbound"
     access = "Allow"
     protocol = "Tcp"
     source_port_range = "*" 
-    destination_port_range = var.jumpbox_port 
-    source_address_prefix = var.jumpbox_ip
-    destination_address_prefix = "*"
-    resource_group_name = var.resource_group_name
-    network_security_group_name = azurerm_network_security_group.monitoring.name
-}
-
-resource "azurerm_network_security_rule" "grafana" {
-    name = "grafana"
-    priority = 110
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*" 
-    destination_port_range = var.grafana_port
+    destination_port_range = var.rdp_port
     source_address_prefix = var.client_ip
     destination_address_prefix = "*"
     resource_group_name = var.resource_group_name
-    network_security_group_name = azurerm_network_security_group.monitoring.name
+    network_security_group_name = azurerm_network_security_group.terraform.name
 }
 
