5a019392a9ceb915db03359b663f2c4c2a190366
kate <23490278+millanie@users.noreply.github.com>
2020-03-18 11:59:20 +0000

    add vm script for terraform

--- terraform/vm/network.tf
+++ terraform/vm/network.tf
@@ -8,7 +8,33 @@ module "network" {
   vnet_name = var.vnet_name
   address_space = var.vnet_cidr
 
-#   subnet_names = var.name
+  subnet_names = var.subnet_names
   subnet_prefixes = var.subnet_prefixes
 }
 
+### attach nsg on the subnet
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-nsg"
+#   resource_group_name = var.resource_group_name
+#   location = var.location
+# 
+#   security_rule {
+#     name                       = ""
+#     priority                   = 100
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "*"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
+# 
+# 
+# resource "azurerm_subnet_network_security_group_association" "example" {
+#     count = length(var.subnet_prefixes)
+# 
+#    subnet_id                 = module.network.vnet_subnets[count.index]
+#    network_security_group_id = azurerm_network_security_group.example.id
+# }
