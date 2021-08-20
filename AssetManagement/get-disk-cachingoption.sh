1ace38daa6386ab8f3659164702f709efcb0500a
kate <23490278+millanie@users.noreply.github.com>
2021-08-20 04:24:43 +0000

    script for disk caching information

---
+++ AssetManagement/get-disk-cachingoption.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+az vm list -o table | awk '{print "az vm show -n " $1 " -g "$2 " --show-details" }' | grep -v "\-\-\-" | grep -v ResourceGroup > /tmp/query.sh
+
+export postfix=" --query '{vmName:name,OsDiskName:storageProfile.osDisk.name, OsDiskCaching:storageProfile.osDisk.caching,DataDiskName:storageProfile.dataDisks[].name,DataDiskCaching:storageProfile.dataDisks[].caching}';echo ',';"
+
+cat /tmp/query.sh | while read line; do echo ${line}$postfix; done > /tmp/vm-details.sh
+
+outfile="/tmp/vm-details.json"
+
+sh /tmp/vm-details.sh > $outfile
+
+sed -i '1i {' $outfile
+echo "}" >> $outfile
+
