2e64b0fbaa0d91615521ac28fa1cf9ef2bb22961
Kate <23490278+millanie@users.noreply.github.com>
2021-09-03 09:17:02 +0900

    Update get-disk-cachingoption.sh

--- AssetManagement/get-disk-cachingoption.sh
+++ AssetManagement/get-disk-cachingoption.sh
@@ -4,9 +4,6 @@ az vm list -o table | awk '{print "az vm show -n " $1 " -g "$2 " --show-details"
 
 export postfix=" --query '{vmName:name,OsDiskName:storageProfile.osDisk.name,OsDiskCaching:storageProfile.osDisk.caching,OsDiskWriteAccelerator:storageProfile.osDisk.writeAcceleratorEnabled,DataDiskName:storageProfile.dataDisks[].name, DataDiskCaching:storageProfile.dataDisks[].caching,DataDiskWriteAccelerator:storageProfile.dataDisks[].writeAcceleratorEnabled}';echo ',';"
 
-vmName:name,OsDiskName:storageProfile.osDisk.name, OsDiskCaching:storageProfile.osDisk.caching,OSwriteAccelerator:storageProfile.osDisk.writeAcceleratorEnabled,DataDiskName:storageProfile.dataDisks[].name,DataDiskCaching:storageProfile.dataDisks[].caching,DatawriteAccelerator:storageProfile.dataDisks[].writeAcceleratorEnabled
-
-
 cat /tmp/query.sh | while read line; do echo ${line}$postfix; done > /tmp/vm-details.sh
 
 outfile="/tmp/vm-details.json"
